184f29323b7bcdddf7438926a916a54e8bf9881e
nastaj <G00424689@atu.ie>
2023-06-30 18:56:36 +0200

    added scripts

---
+++ js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const tabsContainer = document.querySelector(".tabs");
+const scoreEl = document.querySelector(".score__primary");
+
+// Receive data from data.json
+const getData = async function () {
+  try {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Generate markup for each entry in data
+const generateMarkup = async function () {
+  try {
+    const data = await getData();
+    data.forEach((entry) => {
+      const category = entry.category.toLowerCase();
+      const markup = `
+              <div class="tab tab-${category}">
+                <div class="tab__type">
+                  <img
+                    src="assets/images/icon-${category}.svg"
+                    alt="${entry.category}"
+                  />
+                  <p class="tab__type__title--${category}">${entry.category}</p>
+                </div>
+                <p><span class="tab__type__score--current">${entry.score}</span> / 100</p>
+              </div>
+      `;
+      tabsContainer.insertAdjacentHTML("beforebegin", markup);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+generateMarkup();
+
+// Calculate and render the final score
+const setFinalScore = async function () {
+  try {
+    const data = await getData();
+    const scoreFinal = Math.trunc(
+      data.reduce((acc, cur) => acc + cur.score, 0) / data.length
+    );
+
+    scoreEl.textContent = scoreFinal;
+  } catch (err) {
+    console.log(err);
+  }
+};
+setFinalScore();
